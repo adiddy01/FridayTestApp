6c3e52571ae32dd5395188284283a5920cdeea94
adiddy01 <adiddy01@gmail.com>
2019-03-22 14:20:12 -0700

    Initial create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FridayTestMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
